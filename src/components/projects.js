eae451eaefa1345ab7208344c9d9fd89c94d1b55
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-07-27 18:30:47 +0300

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,24 +1,24 @@
-import { graphql, useStaticQuery } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import React from "react"
-import styled from "styled-components"
+// import { graphql, useStaticQuery } from "gatsby"
+// import { GatsbyImage, getImage } from "gatsby-plugin-image"
+// import React from "react"
+// import styled from "styled-components"
 
-const StyledSection = styled.section`
-  .title {
-    margin-bottom: 0;
-  }
-  .subtitle {
-    margin-top: 0;
-  }
-  .highlighted {
-    box-shadow: inset 0 -2.5rem 0 #cdf3e1;
-  }
-`
+// const StyledSection = styled.section`
+//   .title {
+//     margin-bottom: 0;
+//   }
+//   .subtitle {
+//     margin-top: 0;
+//   }
+//   .highlighted {
+//     box-shadow: inset 0 -2.5rem 0 #cdf3e1;
+//   }
+// `
 
-const Projects = ({ content }) => {
-  const { frontmatter } = content
-  let featuredImg = getImage(frontmatter.featuredImage?.childImageSharp?.gatsbyImageData)
-  console.log("u", frontmatter)
+// const Projects = ({ content }) => {
+//   const { frontmatter } = content
+//   let featuredImg = getImage(frontmatter.featuredImage?.childImageSharp?.gatsbyImageData)
+//   console.log("u", frontmatter)
 
   // const data = useStaticQuery(graphql`
   // query {
@@ -44,37 +44,37 @@ const Projects = ({ content }) => {
 
   // console.log("DATA", data);
 
-  return (
+//   return (
 
-    <StyledSection id="projects">
-      <div>
-        {
-          frontmatter.map(node => {
-            return (
-              <div>
-                <h2 className="title">
-                  {node.projectTitle[0]}
-                </h2>
-                {/* <h4 className="subtitle">
-                {node.subtitle}:
-              </h4> */}
-                <h6>{node.subtitleDesc[0]}</h6>
-                <div className="description">
-                  {/* {node.descriptionTitle} */}
-                  <p>{node.description[0]}</p>
-                  <GatsbyImage image={featuredImg} />
-                </div>
-              </div>
-            )
-          }
-          )
-        }
-      </div>
-    </StyledSection>
-  )
-}
+//     <StyledSection id="projects">
+//       <div>
+//         {
+//           frontmatter.map(node => {
+//             return (
+//               <div>
+//                 <h2 className="title">
+//                   {node.projectTitle[0]}
+//                 </h2>
+//                 {/* <h4 className="subtitle">
+//                 {node.subtitle}:
+//               </h4> */}
+//                 <h6>{node.subtitleDesc[0]}</h6>
+//                 <div className="description">
+//                   {/* {node.descriptionTitle} */}
+//                   <p>{node.description[0]}</p>
+//                   <GatsbyImage image={featuredImg} />
+//                 </div>
+//               </div>
+//             )
+//           }
+//           )
+//         }
+//       </div>
+//     </StyledSection>
+//   )
+// }
 
-export default Projects
+// export default Projects
 
 
 // projects klasoru altinda her bir prjoje icin md file olur
