d5d64bfae155370a76d30a3ee581827f0464d82d
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-07-29 22:51:35 +0300

    add button

--- src/components/about.js
+++ src/components/about.js
@@ -11,28 +11,40 @@ const StyledSection = styled.section`
   .highlighted {
     box-shadow: inset 0 -2.5rem 0 #cdf3e1;
   }
+  .btn {
+    height:50px;
+    width:150px;
+    background-color: #cdf3e1;
+    border: 1px solid #b8daca;&:hover{
+      background-color: #b8daca;
+    }
+    border-radius: 8px;
+    cursor: pointer;
+
+  }
 `
 
 const About = ({ content }) => {
-    const { frontmatter, rawMarkdownBody } = content
+  const { frontmatter, rawMarkdownBody } = content
 
-    return (
-        <StyledSection id="about">
-            <h1 className="title">
-                {frontmatter.greetings}{" "}
-                <span role="img" aria-label="emoji">
-                    {frontmatter.emoji}
-                </span>
-                <br />
-                {frontmatter.title}
-            </h1>
-            <h2 className="subtitle">
-                {frontmatter.subtitlePrefix}{" "}
-                <span className="highlighted">{frontmatter.subtitleHighlight}</span>
-            </h2>
-            <div className="description">{rawMarkdownBody}</div>
-        </StyledSection>
-    )
+  return (
+    <StyledSection id="about">
+      <h1 className="title">
+        {frontmatter.greetings}{" "}
+        <span role="img" aria-label="emoji">
+          {frontmatter.emoji}
+        </span>
+        <br />
+        {frontmatter.introduce}
+      </h1>
+      <h2 className="subtitle">
+        {frontmatter.subtitlePrefix}{" "}
+        <span className="highlighted">{frontmatter.subtitleHighlight}</span>
+      </h2>
+      <div className="description">{rawMarkdownBody}</div>
+      <button className="btn">My Projects</button>
+    </StyledSection>
+  )
 }
 
 export default About
